b3eee6bcafa2ff2dfc22b31af24cc81a6994f31b
rizkibagusss <rizkibagusariyanto@gmail.com>
2026-02-25 21:13:39 +0700

    Prepare production deployment

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,15 @@
 export default function HeroSection() {
   return (
     <section className="pt-20 pb-24">
-      <h1 className="text-xl font-medium">Rizki Bagus Ariyanto</h1>
+      <h1 className="text-xl font-medium">Rizki Bagus Ariyanto </h1>
 
-      <p className="mt-1 text-neutral-500 text-lg">Builder</p>
+      <p className="mt-1 text-neutral-500 text-md">
+        IT Infrastructure & Operational
+      </p>
 
       <p className="mt-10 text-neutral-600 text-lg leading-relaxed">
-        Focused on creating intuitive and performant web experiences. Bridging
-        the gap between design and development.
+        Building scalable systems — from infrastructure to web applications.
+        Focused on performance, clean architecture, and continuous learning.
       </p>
     </section>
   );
